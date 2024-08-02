edab494a2a04575af9e02217c77184368917152d
yashtrambadiya <yashtrambadiya008@gmail.com>
2024-08-02 18:25:29 +0530

    resume added

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,7 +13,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/yashtrambadiya",
   resume:
-    "",
+    "https://drive.google.com/file/d/1GVwb9HQcDcmIiP3tYrRP2L9a02c7HHzv/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/yash-trambadiya-926592184/",
   mail: "mailto:gartokn@eliotkids.com",
   facebook: "tel:+919998272625",
